24ac578e0e279ff25e5983ef1c89b912c14c3a7e
Shippu Kumar <Itzshippu@gmail.com>
2025-03-13 16:47:04 +0530

    Updated Server Actions

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,42 +1,11 @@
 import BlogCard from '@/components/Blog/BlogCard'
+import { getAllArticles } from '@/lib/actions/article.action'
 import React from 'react'
 
-const blogs = [
-  {
-    image: 'https://tirzepatyd.store/wp-content/uploads/apollo13_images/20333-7ljgc6rny7rufq07c4ix363kjr7y8vhyvq.jpg',
-    title: 'New Drug for Obesity',
-    date: 'January 15, 2024',
-    description:
-      'A new drug called retatrutyd is showing promising results in the fight against obesity. This article discusses the drug and its potential benefits.',
-    slug:'hamowanie-apetytu-semaglutyd-a-laknienie/'
-  },
-  {
-    image: 'https://tirzepatyd.store/wp-content/uploads/apollo13_images/20333-7ljgc6rny7rufq07c4ix363kjr7y8vhyvq.jpg',
-    title: 'New Drug for Obesity',
-    date: 'January 15, 2024',
-    description:
-      'A new drug called retatrutyd is showing promising results in the fight against obesity. This article discusses the drug and its potential benefits.',
-    slug:'hamowanie-apetytu-semaglutyd-a-laknienie/'
-  },
-  {
-    image: 'https://tirzepatyd.store/wp-content/uploads/apollo13_images/20333-7ljgc6rny7rufq07c4ix363kjr7y8vhyvq.jpg',
-    title: 'New Drug for Obesity',
-    date: 'January 15, 2024',
-    description:
-      'A new drug called retatrutyd is showing promising results in the fight against obesity. This article discusses the drug and its potential benefits.',
-    slug:'hamowanie-apetytu-semaglutyd-a-laknienie/'
-  },
-  {
-    image: 'https://tirzepatyd.store/wp-content/uploads/apollo13_images/20333-7ljgc6rny7rufq07c4ix363kjr7y8vhyvq.jpg',
-    title: 'New Drug for Obesity',
-    date: 'January 15, 2024',
-    description:
-      'A new drug called retatrutyd is showing promising results in the fight against obesity. This article discusses the drug and its potential benefits.',
-    slug:'hamowanie-apetytu-semaglutyd-a-laknienie/'
-  },
-]
 
 const page = async () => {
+  const {articles} = await getAllArticles()
+  
   return (
     <>
       <section className='bg-blue-100 pt-28 pb-12 flex items-center justify-center border-b border-blue-200'>
@@ -44,7 +13,7 @@ const page = async () => {
       </section>
       <main>
         <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 lg:gap-10'>
-          {blogs.map((blog)=> <BlogCard key={blog.id} {...blog}/>)}
+          {articles.map((blog)=> <BlogCard key={blog.slug} {...blog}/>)}
         </section>
       </main>
     </>
--- components/Blog/BlogCard.jsx
+++ components/Blog/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const BlogCard = ({ image, title, date, description, slug }) => {
+const BlogCard = ({ image, title, date, metaDescription, slug }) => {
   return (
     <div className="bg-white border border-gray-300 shadow-md shadow-gray-300 overflow-hidden flex flex-col">
       {image && (
@@ -16,11 +16,11 @@ const BlogCard = ({ image, title, date, description, slug }) => {
         </div>
       )}
       <div className="p-4 flex-1 flex flex-col">
-        {title && <h2 className="text-lg font-semibold">{title}</h2>}
+        {title && <h2 className="text-lg font-semibold"><Link href={"/blogs/"+slug}>{title}</Link></h2>}
         {date && <p className="text-sm text-gray-500">{date}</p>}
         <hr className='border-gray-300 my-2'/>
-        {description && <p className="text-gray-700 flex-1">{description}</p>}
-        <Link href={"blogs/"+slug} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-2 px-4 text-sm hover:cursor-pointer">
+        {metaDescription && <p className="text-gray-700 flex-1">{metaDescription}</p>}
+        <Link href={"/blogs/"+slug} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-2 px-4 text-sm hover:cursor-pointer">
           Read More
         </Link>
       </div>
